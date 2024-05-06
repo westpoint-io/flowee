4d882a8611c7b21896f3cc9bed3daef48fa5f6fb
Camilo-J <josecamilo1902@outlook.com>
2024-05-06 14:40:19 -0500

    chore: refactor homepage styles & update assets files importing

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,43 +1,33 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import './flowee.css';
+import styles from './flowee.module.css';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
-
-      <main className="main-content">
-        <section className="margin">
-          <h1 className="text-5xl margin-bot-1_5">Flowee</h1>
-          <p className="text-lg gray-light margin-bot-1_5 w-500">
-            Generate AWS diagrams using your cloudformation templates as input
-            directly from flowee CLI
-          </p>
-          <section className="flex gap-2 margin-top-1">
-            <a className="github-button" href="https://github.com/westpoint-io/flowee">
-              <span className="text-sm">Github</span>
-              <img
-                className="github-icon"
-                src="/public/github.svg"
-                alt="github icon"
-              />
-            </a>
-            <a className="documentation-button" href="/docs/Getting_Started">
-              <span className="text-sm">Documentation</span>
-              <img
-                className="documentation-icon"
-                src="/public/documentation.svg"
-                alt="documentation icon"
-              />
-            </a>
-          </section>
+    <main className={`${styles['main-content']}`}>
+      <section className={`${styles.margin}`}>
+        <h1 className={`${styles['text-5xl']} ${styles['margin-bot-1_5']}`}>Flowee</h1>
+        <p className={`${styles['text-lg']} ${styles['gray-light']} ${styles['margin-bot-1_5']} ${styles['w-500']}`}>
+          Generate AWS diagrams using your cloudformation templates as input directly from flowee CLI
+        </p>
+        <section className={`${styles.flex} ${styles['gap-2']} ${styles['margin-top-1']}`}>
+          <a
+            className={`${styles['github-button']}`}
+            href="https://github.com/westpoint-io/flowee"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <span className={`${styles['text-sm']}`}>Github</span>
+            <img className={`${styles['github-icon']}`} src={useBaseUrl('public/github.svg')} alt="github icon" />
+          </a>
+          <a className={`${styles['documentation-button']}`} href={useBaseUrl('/docs/Getting_Started')}>
+            <span className={`${styles['text-sm']}`}>Documentation</span>
+            <img className={`${styles['documentation-icon']}`} src={useBaseUrl('/public/documentation.svg')} alt="documentation icon" />
+          </a>
         </section>
-        <section className="">
-          <img
-            className="rounded-lg shadows-gift"
-            src="/public/terminal.gif"
-            alt=""
-          />
-        </section>
-      </main>
+      </section>
+      <section className="">
+        <img className={`${styles['rounded-lg']} ${styles['shadows-gift']}`} src={useBaseUrl('/public/terminal.gif')} alt="terminal gif" />
+      </section>
+    </main>
   );
 }
